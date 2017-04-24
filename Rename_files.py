42a1e1a6dd1df1a78b1bd8f47e569cf35a04a515
MUstaf Eli <elimustaf@gmail.com>
2017-04-23 23:21:17 -0700

    add file path

--- Rename_files.py
+++ Rename_files.py
@@ -1,7 +1,9 @@
-
+import os
 
 def rename_files():
     #get file names from a folder
+    file_list = os.listdir(r"C:\Users\TheHomie\Documents\Udacity\Github\python\Rename_file\prank")
+    print(file_list)
 
     #for each file, rename filename
 
